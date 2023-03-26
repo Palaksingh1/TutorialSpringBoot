c0eb8f4d29a9c57dc433d4604229685f541f371b
Palaksingh1 <117344679+Palaksingh1@users.noreply.github.com>
2023-03-26 11:19:38 +0530

    Add files via upload

---
+++ tutorialmanagement/src/main/java/edu/tutorial/exception/GlobalExceptionForTutorial.java
@@ -0,0 +1,11 @@
+
+package edu.tutorial.exception;
+
+public class GlobalExceptionForTutorial  extends Exception{
+	
+	public GlobalExceptionForTutorial(String message)
+	{
+		super(message);
+	}
+
+}
